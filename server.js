a4f87ae18828c1471ae43a22bdb2b9d17446e383
Kevin Stotz <kevin_stotz@yahoo.com>
2018-11-14 19:50:11 +0000

    change server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 const app = express();
 
-app.listen(10100, () => console.log('Gator app listening on port 10100!'));
+app.listen(10101, () => console.log('Gator app listening on port 10101!'));
 
 app.use(express.static(__dirname )); //__dir and not _dir
 app.use((req, res, next) => {
